525c0271868cfa581d5a71b51955ad367705d978
RyderBlack <ryhad.boughanmi@gmail.com>
2017-09-17 12:14:41 +0200

    updating the app 🎧

--- Bill Says/ViewController.swift
+++ Bill Says/ViewController.swift
@@ -19,7 +19,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var celebrities = ["the Steve Jobs", "the Stephen Curry", "the Madonna", "the Lagarfeld", "the Scarlett Johansson"]
+    var places = ["of the fifth street", "of the Apple Store", "of Hollywood", "of the NBA", "of Disneyland"]
+    
+    
 
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBAction func changeQuote() {
+        
+        // On séléctionne un élément alétoire parmi les célébrités
+        let randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let celebrity = celebrities[randomIndex1]
+        print(celebrity)
+        
+        // On séléctionne un élément aléatoire parmi les activités
+        let randomIndex2 = Int(arc4random_uniform(UInt32(places.count)))
+        let place = places[randomIndex2]
+        print(place)
+        
+        quoteLabel.text = "You are " + celebrity + " " + place
+    }
 
 }
 
